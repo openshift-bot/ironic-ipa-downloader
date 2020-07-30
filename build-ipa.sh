944c089aea74c083f41ace5738552686de987a80
Riccardo Pittau <elfosardo@gmail.com>
2020-07-30 14:42:01 +0200

    Use dnf to install packages in the ipa ramdisk
    Move the logic to modify the ipa-ramdisk to the chrooted.sh script
    that is executed in a chroot environment.
    This allows us to use native commands directly in the ipa-ramdisk
    image, like dnf, and exit correctly in case of failures.
    Also provide a list of packages installed in the ipa-ramdisk.

--- build-ipa.sh
+++ build-ipa.sh
@@ -10,38 +10,33 @@ chmod 755 $TMPDIR $TMPDIR_RAMDISK
 cd $TMPDIR
 tar -xf /usr/share/rhosp-director-images/ironic-python-agent-${IPA_RAMDISK_VERSION}.${IPA_RAMDISK_ARCH}.tar
 cd $TMPDIR_RAMDISK
+
+# Extract the ipa-ramdisk filesystem
 /usr/lib/dracut/skipcpio $TMPDIR/ironic-python-agent.initramfs | zcat | cpio -ivd
-# NOTE(elfosardo) we could inject a list of packages that we want to add, based
-# on what we download in the main image and call this part only if we actually
-# have packages in the list.
-# Also version tagging is something we should consider.
-# And cookies.
-mv /tmp/packages/*.rpm .
 
 # NOTE(elfosardo) to prevent the ``BDB0091 DB_VERSION_MISMATCH: Database
 # environment version mismatch`` error, we need to remove the rpm lock.
 rm var/lib/rpm/.rpm.lock
 
-chroot . /bin/bash -x <<'EOF'
-rpm -Uvh \
-openstack-ironic-python-agent*.rpm \
-python3-bcrypt*.rpm \
-python3-ironic-python-agent*.rpm \
-python3-ironic-lib*.rpm || { exit 1; }
+# Prepare the chroot environment
+cp /etc/resolv.conf etc/resolv.conf
+mv etc/yum.repos.d/* .
+cp /etc/yum.repos.d/* etc/yum.repos.d/
+cp /usr/local/bin/chrooted.sh .
 
-rm -f *.rpm
+# Modify the ipa-ramdisk in chroot
+chroot . ./chrooted.sh
 
-# Update netconfig to use MAC for DUID/IAID combo (same as RHCOS)
-# FIXME: we need an alternative of this packaged
-mkdir -p /etc/NetworkManager/conf.d /etc/NetworkManager/dispatcher.d
-echo -e '[main]\ndhcp=dhclient\n[connection]\nipv6.dhcp-duid=ll' > /etc/NetworkManager/conf.d/clientid.conf
-echo -e '[[ "$DHCP6_FQDN_FQDN" =~ - ]] && hostname $DHCP6_FQDN_FQDN' > /etc/NetworkManager/dispatcher.d/01-hostname
-chmod +x /etc/NetworkManager/dispatcher.d/01-hostname
+# Provide list of packages installed in the ipa-ramdisk
+cp ipa-ramdisk-pkgs-list.txt /var/tmp/
 
-EOF
-
+# Compress the ipa-ramdisk and copy it in the right place
 find . 2>/dev/null | cpio -c -o | gzip -8  > /var/tmp/ironic-python-agent.initramfs
 cp $TMPDIR/ironic-python-agent.kernel /var/tmp/
+
+# Clean temp directories
+rm -fr $TMPDIR $TMPDIR_RAMDISK
+
+# Debug Steps
 cd /var/tmp
 ls -la
-rm -fr $TMPDIR $TMPDIR_RAMDISK
